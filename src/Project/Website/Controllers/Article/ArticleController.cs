e0e735022720f999af362ce90e219b83256c4fc7
Hetal Dave <hetal.dave@verndale.com>
2021-03-06 00:12:23 -0500

    Article & Solution Set up.

---
+++ src/Project/Website/Controllers/Article/ArticleController.cs
@@ -0,0 +1,15 @@
+﻿using Feature.Articles.Models;
+using Sitecore.Mvc.Controllers;
+using System.Web.Mvc;
+
+namespace Website.Controllers.Article
+{
+    public class ArticleController : SitecoreController
+    {
+        public ActionResult Article()
+        {
+            var articleModel = new ArticleModel(Sitecore.Context.Item);
+            return View("~/Views/Article/ArticlePage.cshtml", articleModel);
+        }
+    }
+}
